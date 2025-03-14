c9de42a30c8644f39bb8699a4c77a57a37d081c7
adamrifs <rifsadam81@gmail.com>
2025-03-15 00:31:59 +0530

    cors updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
+    origin: ['http://localhost:5174', 'http://localhost:5173','https://msbline.vercel.app/'],
     credentials: true
 }))
 app.use(cookieParser())
